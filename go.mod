60f7ff7b4b46250f3fede9aa53677637516fc460
mark-summerfield <mark@qtrac.eu>
2023-03-24 09:04:03 +0000

    can list .odt contents

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/mark-summerfield/odtquery
 
 go 1.20
+
+replace github.com/mark-summerfield/odt => /home/mark/app/go/odt/lib
+
+require (
+	github.com/mark-summerfield/clip v0.8.0
+	github.com/mark-summerfield/gong v1.0.1
+	github.com/mark-summerfield/gset v0.8.0
+	github.com/mark-summerfield/odt v0.0.0-20230324075126-14fbdf3ef93c
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
+)
+
+require (
+	github.com/kopoli/go-terminal-size v0.0.0-20170219200355-5c97524c8b54 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- odtquery.go
+++ odtquery.go
@@ -5,12 +5,119 @@ package main
 
 import (
 	_ "embed"
+	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/mark-summerfield/clip"
+	"github.com/mark-summerfield/gong"
+	"github.com/mark-summerfield/gset"
+	"github.com/mark-summerfield/odt"
+	"golang.org/x/exp/slices"
 )
 
 //go:embed Version.dat
 var Version string
 
+var expectedFiles = gset.New("content.xml", "META-INF/manifest.xml",
+	"meta.xml", "mimetype", "styles.xml")
+
 func main() {
-	fmt.Printf("Hello odtquery v%s\n", Version)
+	config := getConfig()
+	if len(config.filenames) == 1 {
+		if err := process(config.filenames[0], config, false); err != nil {
+			fmt.Printf("error: %s\n", err)
+		}
+	} else {
+		for _, filename := range config.filenames {
+			fmt.Println("file:", gong.Bold(filename))
+			if err := process(filename, config, true); err != nil {
+				fmt.Printf("error: %s\n", err)
+			}
+		}
+	}
+}
+
+func process(filename string, config *config, indent bool) error {
+	doc, err := odt.Open(filename)
+	if err != nil {
+		return err
+	}
+	if config.verify {
+		verify(doc, indent)
+	}
+	if config.list {
+		list(doc, indent)
+	}
+	return nil
+}
+
+func verify(doc *odt.Odt, indent bool) {
+	actualFiles := gset.New[string]()
+	for _, file := range doc.Files {
+		if expectedFiles.Contains(file.Name) {
+			actualFiles.Add(file.Name)
+		}
+	}
+	if indent {
+		fmt.Print("  ")
+	}
+	if len(actualFiles) < len(expectedFiles) {
+		missing := expectedFiles.Difference(actualFiles).ToSlice()
+		slices.SortFunc(missing, func(a, b string) bool {
+			return strings.ToLower(a) < strings.ToLower(b)
+		})
+		fmt.Print(gong.Italic("failed verification — missing files:"))
+		for _, name := range missing {
+			fmt.Print(" ", name)
+		}
+		fmt.Println("")
+	} else {
+		fmt.Println(gong.Italic("verified — contains all essential files"))
+	}
+}
+
+func list(doc *odt.Odt, indent bool) {
+	for _, file := range doc.Files {
+		if indent {
+			fmt.Print("  ")
+		}
+		fmt.Print(file.Name)
+		if len(file.Text) == 0 {
+			fmt.Println(" (empty)")
+		} else {
+			fmt.Printf(" (%s bytes)\n", gong.Commas(len(file.Text)))
+		}
+	}
+}
+
+func getConfig() *config {
+	parser := makeParser()
+	listOpt := parser.Flag("list",
+		"List each .odt file's contents. [default]")
+	verifyOpt := parser.Flag("verify", "Verify each .odt file's contents.")
+	if err := parser.Parse(); err != nil {
+		parser.OnError(err)
+	}
+	config := &config{filenames: parser.Positionals,
+		list: listOpt.Value(), verify: verifyOpt.Value()}
+	if !(config.list || config.verify) {
+		parser.OnError(errors.New("error: no action specified"))
+	}
+	return config
+}
+
+func makeParser() *clip.Parser {
+	parser := clip.NewParserVersion(Version)
+	parser.PositionalHelp = ".odt files to query"
+	parser.PositionalCount = clip.OneOrMorePositionals
+	_ = parser.SetPositionalVarName("ODT_FILE")
+	parser.LongDesc = "Queries .odt files."
+	return &parser
+}
+
+type config struct {
+	list      bool
+	verify    bool
+	filenames []string
 }
